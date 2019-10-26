050c7c9dfe5b540cd06afeeaf3cce25b918d1848
Romulo Rocha <romulorocha063@gmail.com>
2019-10-26 12:24:36 -0300

    root path in src

--- mobile-react-native/.eslintrc.js
+++ mobile-react-native/.eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': ['error', {extensions: ['.js', '.jsx']}],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
---
+++ mobile-react-native/src/index.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { SafeAreaView, Text, StatusBar } from 'react-native';
+
+const App = () => (
+  <>
+    <StatusBar barStyle="dark-content" />
+    <SafeAreaView>
+      <Text>Hello CREDITAS</Text>
+    </SafeAreaView>
+  </>
+);
+
+export default App;
